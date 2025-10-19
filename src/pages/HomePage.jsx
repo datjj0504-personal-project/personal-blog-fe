559b4910dbfa9e307fbd7f01303430de9f0ddf27
datnt <tiendatjj0504>
2025-10-19 17:18:34 +0700

    Commit 20251019

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./HomePage.css";
+
+function HomePage() {
+	return (
+		<div className="homepage">
+			<div className="button-container">
+				<button className="btn sign-in">Sign In</button>
+				<button className="btn sign-up">Sign Up</button>
+			</div>
+		</div>
+	);
+}
+
+export default HomePage;
